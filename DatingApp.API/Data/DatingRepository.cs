1d920d484ee00259ef824b50c7006b1ccc9227fa
porfirioR <prezgonzlez.por@gmail.com>
2020-01-12 18:46:33 -0300

    End of section 8

---
+++ DatingApp.API/Data/DatingRepository.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DatingApp.API.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DatingRepository : IDatingRepository
+    {
+        private readonly DataContext _contex;
+
+        public DatingRepository(DataContext context)
+        {
+            _contex = context;
+        }
+        public void Add<T>(T entity) where T : class
+        {
+            _contex.Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            _contex.Remove(entity);
+        }
+
+        public async Task<User> GetUser(int id)
+        {
+            var user = await _contex.Users.Include(p => p.Photos).FirstOrDefaultAsync(u => u.Id == id);
+            return user;
+        }
+
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            var users = await _contex.Users.Include(p => p.Photos).ToListAsync();
+            return users;
+        }
+
+        public async Task<bool> SaveAll()
+        {
+            return await _contex.SaveChangesAsync() > 0;
+        }
+    }
+}
